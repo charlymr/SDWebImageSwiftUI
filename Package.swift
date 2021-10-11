4c60a0926584370dc224f8100720562481e3926e
MARTIN Denis <denis@irlmobile.com>
2021-10-11 20:54:28 +0200

    Updated tools

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/Crate-AS/SDWebImage.git", .branch("master"))
+        .package(url: "https://github.com/charlymr/SDWebImage.git", .branch("master"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
